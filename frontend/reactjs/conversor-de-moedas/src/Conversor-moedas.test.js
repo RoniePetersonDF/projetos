dca2df1a78ff58e341ab8b119e5b6f0b3d02d01b
Ronie Peterson <roniepetersondf@gmail.com>
2020-10-16 18:22:24 -0300

    add html ao react

--- frontend/reactjs/conversor-de-moedas/src/Conversor-moedas.test.js
+++ frontend/reactjs/conversor-de-moedas/src/Conversor-moedas.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import ConversorMoedas from './Conversor-moedas';
 
-test('renders learn react link', () => {
+test('Deve renderizar a página sem erros', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
