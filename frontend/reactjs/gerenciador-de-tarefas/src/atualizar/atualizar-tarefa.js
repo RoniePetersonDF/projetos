c64f986b5f4e6f3758b088c77015ba0c2c411e3f
Ronie Peterson <roniepetersondf@gmail.com>
2020-10-22 16:44:49 -0300

    Criando componente de atualizar tarefa

---
+++ frontend/reactjs/gerenciador-de-tarefas/src/atualizar/atualizar-tarefa.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AtualizarTarefa from './atualizar-tarefa';
+
+describe('Teste do componente atualizar tarefa', () =>{
+
+    it('deve renderizar o componente sem erro', () => {
+        cont div = document.createElement('div');
+        ReactDOM.render(<AtualizarTarefa id={1} />, div);
+        ReactDOM..unmountComponentAtNode(div);
+    });
+});
--- frontend/reactjs/gerenciador-de-tarefas/src/gerenciador-tarefas.js
+++ frontend/reactjs/gerenciador-de-tarefas/src/gerenciador-tarefas.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './gerenciador-tarefas.css';
 import { useRoutes } from 'hookrouter';
 
+import ListarTarefas from './listar/listar-tarefas';
+import CadastrarTarefa from './listar/cadastrar-tarefa';
+import AtualizarTarefa from './listar/atualizar-tarefa';
+
 const routes = {
   '/': () => <ListarTarefas />,
   '/cadastrar': () => <CadastrarTarefa />,
-  // '/atualizar/:id': ({id}) => <AtualizarTarefa id={id} />
+  '/atualizar/:id': ({id}) => <AtualizarTarefa id={id} />
 };
 
 function GerenciadorTarefas() {
